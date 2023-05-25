0c87412398b5dd95ae461f6bfbb98c6ea070f019
plestau <pablitolestau@hotmail.com>
2023-05-25 09:58:06 +0200

    revierte

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,10 @@ use App\Models\User;
 
 class PostController extends Controller
 {
-
+    public function __construct()
+{
+    $this->middleware('auth');
+}
 
     /**
      * Display a listing of the resource.
