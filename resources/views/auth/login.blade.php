2192376e203e03d62178a36413a8dd5b4182955f
plestau <pablitolestau@hotmail.com>
2023-05-25 09:48:08 +0200

    login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,91 @@
-<template>
-    <div class="container mx-auto">
-        <div class="max-w-sm mx-auto mt-32">
-            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div class="mb-4">
-                    <h1 class="text-2xl font-bold text-gray-700 text-center">Iniciar sesión</h1>
-                </div>
-                <login :errors="errors" :session="session" />
+<script setup>
+    import Checkbox from '@/Components/Checkbox.vue';
+    import GuestLayout from '@/Layouts/GuestLayout.vue';
+    import InputError from '@/Components/InputError.vue';
+    import InputLabel from '@/Components/InputLabel.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import TextInput from '@/Components/TextInput.vue';
+    import { Head, Link, useForm } from '@inertiajs/vue3';
+    
+    defineProps({
+        canResetPassword: Boolean,
+        status: String,
+    });
+    
+    const form = useForm({
+        email: '',
+        password: '',
+        remember: false,
+    });
+    
+    const submit = () => {
+        form.post(route('login'), {
+            onFinish: () => form.reset('password'),
+        });
+    };
+    </script>
+    
+    <template>
+        <GuestLayout>
+            <Head title="Log in" />
+    
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
             </div>
-        </div>
-    </div>
-</template>
-
-<script>
-import Login from '@/Pages/Auth/Login.vue'; // Importamos el componente Login
-
-export default {
-    components: {
-        Login, // Registramos el componente Login
-    },
-
-    props: {
-        errors: Object,
-        session: Object,
-    },
-};
-</script>
+    
+            <form @submit.prevent="submit">
+                <div>
+                    <InputLabel for="email" value="Email" />
+    
+                    <TextInput
+                        id="email"
+                        type="email"
+                        class="mt-1 block w-full"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    />
+    
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+    
+                <div class="mt-4">
+                    <InputLabel for="password" value="Password" />
+    
+                    <TextInput
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                    />
+    
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+    
+                <div class="block mt-4">
+                    <label class="flex items-center">
+                        <Checkbox name="remember" v-model:checked="form.remember" />
+                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    </label>
+                </div>
+    
+                <div class="flex items-center justify-end mt-4">
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Forgot your password?
+                    </Link>
+    
+                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Log in
+                    </PrimaryButton>
+                </div>
+            </form>
+        </GuestLayout>
+    </template>
+    
