ce50d51075151787dffd44e0c9a0a1e7e8b401c6
plestau <pablitolestau@hotmail.com>
2023-05-25 09:55:27 +0200

    como funcione

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,10 +9,7 @@ use App\Models\User;
 
 class PostController extends Controller
 {
-    public function __construct()
-{
-    $this->middleware('auth');
-}
+
 
     /**
      * Display a listing of the resource.
